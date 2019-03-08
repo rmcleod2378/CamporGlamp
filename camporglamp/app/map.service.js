12327aa692aefe8fef890a9c99ccdb2958d3fb14
Antheny Wells <anthenywells@Go-away.local>
2019-03-08 14:17:46 -0500

    google maps core functionality

--- camporglamp/app/map.service.js
+++ camporglamp/app/map.service.js
@@ -4,10 +4,10 @@ function MapService($http) {
   const self = this
   self.jsonPayload = null;
   self.campSearch = () => {
-    return $http({
-      method: "GET", 
-      url: `https://maps.googleapis.com/maps/api/js?key=AIzaSyDF6p7FK63oG_e_XmeQybRKRKfB8qG6SEU&callback=initMap` 
-    })
+    // return $http({
+    //   method: "GET", 
+    //   url: `https://maps.googleapis.com/maps/api/js?key=AIzaSyDF6p7FK63oG_e_XmeQybRKRKfB8qG6SEU&callback=initMap` 
+    // })
   };
 }
 
