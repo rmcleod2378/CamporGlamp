3e160bec6d949c802e05846adb742a5db54bd422
Cam <cameronmcouch@gmail.com>
2019-03-08 10:53:31 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("./app"));
+app.use(express.json());
+
 const port = process.env.PORT || 8080; 
 app.listen(port, _ => console.log("Server is running on port 8080"));
