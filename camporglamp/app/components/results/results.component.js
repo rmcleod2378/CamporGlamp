8b78f901f90dfb385871acbaaf56a11589f87669
Antheny Wells <anthenywells@Go-away.local>
2019-03-11 13:34:20 -0400

    refactored SearchService, displayed results

--- camporglamp/app/components/results/results.component.js
+++ camporglamp/app/components/results/results.component.js
@@ -1,38 +1,38 @@
 "use strict";
 const results = {
   templateUrl: `app/components/results/results.html`,
-  controller: ['$scope', function ($scope) {
+  controller: ['SearchService', function (SearchService) {
     const vm = this;
+    vm.results = SearchService.getData()
+    vm.campresults = vm.results;
 
-    vm.map;
     vm.initMap = function () {
       vm.map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 12, lng: -12 },
         zoom: 12
       })
     };
-
     vm.initMap();
 
-    var cities = "Atlanta, USA";
-    var geocoder = new google.maps.Geocoder();
+    // var cities = "Atlanta, USA";
+    // var geocoder = new google.maps.Geocoder();
 
-    $scope.markers = [];
+    // $scope.markers = [];
 
-    var createMarker = function (info) {
-      var marker = new google.maps.Marker({
-        map: $scope.map,
-        position: new google.maps.LatLng(info.lat(), info.lng())
-      });
-    }
+    // var createMarker = function (info) {
+    //   var marker = new google.maps.Marker({
+    //     map: $scope.map,
+    //     position: new google.maps.LatLng(info.lat(), info.lng())
+    //   });
+    // }
 
-    geocoder.geocode({ 'address': cities }, function (results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        newAddress = results[0].geometry.location;
-        $scope.map.setCenter(newAddress);
-        createMarker(newAddress)
-      }
-    });
+    // geocoder.geocode({ 'address': cities }, function (results, status) {
+    //   if (status == google.maps.GeocoderStatus.OK) {
+    //     newAddress = results[0].geometry.location;
+    //     $scope.map.setCenter(newAddress);
+    //     createMarker(newAddress)
+    //   }
+    // });
   }]
 };
 
--- camporglamp/app/search.service.js
+++ camporglamp/app/search.service.js
@@ -1,16 +1,21 @@
 "use strict";
 function SearchService($http, $location) {
-    const self = this;
-    const key = "OP2F1MT0eeFGORzUkOdqULyY1Ed9dB9hdDiFIlD8"
-    self.get = function() {
-      $location.path("/results")
-        return $http({
-            method:'GET',
-            url:`https://api.nps.gov/api/v1/campgrounds?stateCode=MI&limit=5&api_key=${key}`
-        });
-    };
-
-};
-
+  const key = "OP2F1MT0eeFGORzUkOdqULyY1Ed9dB9hdDiFIlD8";
+  const self = this;
+  self.campresults = null;
+  self.get = function() {
+    return $http({
+      method: "GET",
+      url: `https://api.nps.gov/api/v1/campgrounds?stateCode=MI&limit=5&api_key=${key}`
+    }).then(function(response) {
+      self.campresults = response.data.data;
+      $location.path("/results");
+      return self.campresults;
+    });
+  };
+  self.getData = () => {
+    return self.campresults;
+  };
+}
 
 angular.module("App").service("SearchService", SearchService);
