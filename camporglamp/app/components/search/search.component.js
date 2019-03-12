5972ffc19138c69d1d2ea19c61d1445877b48e3e
Justin <prankjt@gmail.com>
2019-03-12 12:36:56 -0400

    early styling of the app, mobile first

--- camporglamp/app/components/search/search.component.js
+++ camporglamp/app/components/search/search.component.js
@@ -3,11 +3,16 @@ const search = {
     templateUrl: "app/components/search/search.html",
     controller: ['SearchService', '$location', function (SearchService, $location) {
         const vm = this;
+        vm.showFooter = false;
         //this method is a PROPERTY of our search controller. Onclick (in HTML) it calls the GET from our service.
         vm.campSearch = function () {
             //our service get method
             SearchService.get();
         };
+        vm.show = () => {
+            vm.showFooter = !vm.showFooter;
+            console.log("click you")
+          }
     }]
 };
 
