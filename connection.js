cc97931bfe8b254129c2f0a14ae9b587879069e1
Justin <prankjt@gmail.com>
2019-03-14 08:40:20 -0400

    added db to heroku, added .env,  pulling data from database confirmed in console.log

--- connection.js
+++ connection.js
@@ -1,12 +1,20 @@
 "use strict";
 //connection object that is building a way to communicate to the database; module uses a pool to save time by reusing connections
-const { Pool } = require("pg");
-const credentials = new Pool({
-user: "postgres",
-password: "password",
-host: "localhost",
-port: 5432,
-database: "glampDB",
-ssl: false
-});
-module.exports = credentials;  //exporting to the routes.js
+const pg = require("pg");
+const url = require("url");
+try {
+  require("dotenv").config();
+} catch (e) {
+  console.log(e);
+}
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(":");
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+  ssl: params.hostname !== "localhost"
+};
+module.exports = new pg.Pool(config);
