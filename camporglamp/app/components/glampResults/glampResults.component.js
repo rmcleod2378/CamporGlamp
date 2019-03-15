95fb450322616cbbbef59ef55236a43b9d87a424
Antheny Wells <anthenywells@Go-away.local>
2019-03-15 14:06:16 -0400

    results/map toggle buttons for mobile/tablet

--- camporglamp/app/components/glampResults/glampResults.component.js
+++ camporglamp/app/components/glampResults/glampResults.component.js
@@ -6,6 +6,27 @@ const glampResults = {
       vm.glampresults = SearchService.getGData();
       vm.gsiteCoord = SearchService.createGCoord();
 
+
+      const map = angular.element(document.querySelector("#map"));
+      const results = angular.element(document.querySelector(".results"));
+      const fixedRight = angular.element(document.querySelector(".fixedRight"));
+      const fixedLeft = angular.element(document.querySelector(".fixedLeft"));
+      vm.showMap = function() {
+          map.css("display", "flex");
+          fixedLeft.css("display", "flex");
+          fixedRight.css("display", "none");
+          results.css("display", "none");
+      }
+      vm.showResults = function() {
+          map.css("display", "none");
+          fixedLeft.css("display", "none");
+          fixedRight.css("display", "flex");
+          results.css("display", "flex");
+      }
+
+
+
+
       // draws map for glampsites
       vm.initMap = function() {
         vm.map = new google.maps.Map(document.getElementById("map"), {
