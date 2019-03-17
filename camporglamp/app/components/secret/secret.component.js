df81e5981316a88e1741f69de5572593d003536d
Cam <cameronmcouch@gmail.com>
2019-03-17 09:10:52 -0400

    mario secret page is good to go. Music plays auto/loop. Need to hook up to the main page if we want to use it

--- camporglamp/app/components/secret/secret.component.js
+++ camporglamp/app/components/secret/secret.component.js
@@ -2,6 +2,9 @@ const secret = {
     templateUrl:'app/components/secret/secret.html',
     controller: ["SearchService", "$location", function(SearchService, $location) {
         const vm = this;
+        vm.showyoshi = false;
+        vm.showforest = false;
+        vm.showcastle = false;
     }]
 };
 
