7b631f82f10ba49378d44d65319c64da662e5eea
Justin <prankjt@gmail.com>
2019-03-19 14:29:52 -0400

    favorites now delete,  doubles are blocked, and simple styling....giggity

--- camporglamp/app/components/favorites/favorite.component.js
+++ camporglamp/app/components/favorites/favorite.component.js
@@ -1,35 +1,32 @@
 "use strict";
 
 const favorite = {
-    
-  templateUrl:"app/components/favorites/favorite.html",
-  controller: ["SearchService", function(SearchService) {
-      const vm = this;
-      vm.shown = false;
-      vm.$onInit = () => {
-          vm.faveArray = SearchService.getFave();
-      }
-      // delete from faveArray
-      vm.delete = (idx) => {
-          AppService.deleteTempFav(idx);
-          vm.faveArray = SearchService.getFave();
-      }
-      // // pop up behavior within faveArray
-      // vm.show = (item) => {
-      //     vm.focused = item;
-      //     vm.shown = true;
-    // }
-      // behavior for faving from search-details template
-      vm.setTempFave = (newArray) => {
-          vm.faveArray = newArray;
-      }
 
-      vm.getFave = function(sites) {
-        console.log(sites);
-        SearchService.getFave(sites);
-      };
-         
+  templateUrl: "app/components/favorites/favorite.html",
+  controller: ["SearchService", function (SearchService) {
+    const vm = this;
+    vm.shown = false;
+    vm.$onInit = () => {
+      vm.faveArray = SearchService.getFave();
+    }
+    // delete from faveArray
+    vm.delete = (idx) => {
+      SearchService.deleteTempFav(idx);
+      vm.faveArray = SearchService.getFave();
+    }
+    // behavior for faving from search-details template
+    vm.setTempFave = (newArray) => {
+      vm.faveArray = newArray;
+    }
+    vm.glampSearch = function () {
+      //our service get method
+      SearchService.setGlamp();
+    };
 
+    // vm.getFave = function(sites) {
+    //   console.log(sites);
+    //   SearchService.getFave(sites);
+    // };
   }]
 };
 
