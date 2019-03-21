9f62a9485e07a3bd18fbbf79f414824b8e976d2c
Cam <cameronmcouch@gmail.com>
2019-03-20 20:05:26 -0400

    mario page is 100% and added music to game of thrones page

--- camporglamp/app/components/secret/secret.component.js
+++ camporglamp/app/components/secret/secret.component.js
@@ -4,7 +4,9 @@ const secret = {
     templateUrl:'app/components/secret/secret.html',
     controller: ["SearchService", "$location", function(SearchService, $location) {
         const vm = this;
+        vm.showyoshi = false;
+        vm.showforest = false;
+        vm.showcastle = false;
     }]
-};
-
+}
 angular.module("App").component("secret", secret);
